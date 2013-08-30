b7d42a3826ad6da6b435106f8b9b04b32a75fbff
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2013-08-29 20:10:42 -0400

    css

--- extension/inject.js
+++ extension/inject.js
@@ -9236,8 +9236,24 @@ $("body *").addClass("catify");
 
 var elems = document.getElementsByTagName('img');
 for(var i = 0; i < elems.length; i++) { 
- var width = elems[i].getAttribute('width');
- var height= elems[i].getAttribute('height');
+ var width = elems[i].offsetWidth;
+ var height= elems[i].offsetHeight;
+ if (width == null || height == null){
+  width = 300;
+  height=400;
+ }
+ elems[i].setAttribute("src", "http://placekitten.com/"+width+"/"+height);
+
+ }
+
+});
+$(document).ajaxComplete(function() {
+$("body *").addClass("catify");
+
+var elems = document.getElementsByTagName('img');
+for(var i = 0; i < elems.length; i++) { 
+ var width = elems[i].offsetWidth;
+ var height= elems[i].offsetHeight;
 
  elems[i].setAttribute("src", "http://placekitten.com/"+width+"/"+height);
 
